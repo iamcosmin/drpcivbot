d0b94a18f878bfb6b6715724537cb4760524a6d2
iamcosmin <47219753+iamcosmin@users.noreply.github.com>
2023-01-28 20:38:17 +0200

    Improved questions, added experimental quiz mode

--- lib/app.ts
+++ lib/app.ts
@@ -1,6 +1,7 @@
 import Express from 'express';
 import { questionList } from './docs';
-import { Telegraf } from 'telegraf';
+import { Context, NarrowedContext, Telegraf } from 'telegraf';
+import { Message } from 'telegraf/typings/core/types/typegram';
 require('dotenv').config()
 
 function log(tech: String, val: String) {
@@ -12,8 +13,8 @@ function sleep(ms: number) {
 }
 
 function generateQuestion() {
-    var qId = Math.floor(Math.random() * 1148);
-    var q = questionList.at(qId);
+    const qId = Math.floor(Math.random() * 1148);
+    const q = questionList.at(qId);
     return q;
 }
 
@@ -53,25 +54,104 @@ const bot = new Telegraf(token!)
 
 log('Bot', 'Polling started successfully.')
 
-bot.command('i', async (ctx) => {
-    const question = generateQuestion()
-    if (question?.questionImage != null) {
-        await ctx.sendPhoto('https://raw.githubusercontent.com/iamcosmin/drpcivbot/main/assets/image/' + question.questionImage)
-    }
-    const message = await ctx.sendMessage('5', {message_thread_id: ctx.message.message_thread_id})
-    await sleep(1000)
-    for (var i = 1; i < 5; i++) {
-        bot.telegram.editMessageText(message.chat.id, message.message_id, undefined, (5 - i).toString())
-        await sleep(1000)
-    }
-    bot.telegram.editMessageText(message.chat.id, message.message_id, undefined,
-`Alege varianta corectă în sondajul de mai jos.
+type Question = {
+    id: number;
+    question: string;
+    questionImage: null;
+    answ1: string;
+    answ2: string;
+    answ3: string;
+    correct: string;
+} | {
+    id: number;
+    question: string;
+    questionImage: string;
+    answ1: string;
+    answ2: string;
+    answ3: string;
+    correct: string;
+} | undefined;
 
-    Întrebare: <b>${question?.question}</b>
+function questionText(question: Question): string {
+    return `Întrebare: <b>${question?.question}</b>
         A. ${question?.answ1}
         B. ${question?.answ2}
-        C. ${question?.answ3}`, { parse_mode: 'HTML' })
-    ctx.sendQuiz('Alege varianta corectă.', ['A', 'B', 'C', 'AB', 'BC', 'AC', 'ABC'], {is_anonymous: false, correct_option_id: answerStrToInt(question?.correct), message_thread_id: ctx.message.message_thread_id, });
+        C. ${question?.answ3}`
+}
+
+async function sendQuestion(ctx: Context, msg: Message, isCountdown: boolean): Promise<(number)[]> {
+    const question = generateQuestion()
+
+    const countdown = await ctx.sendMessage(`Întrebarea va fi afișată în <b>5</b> secunde.`, { parse_mode: 'HTML' })
+    await sleep(1000)
+    for (var i = 4; i > 0; i--) {
+        await ctx.telegram.editMessageText(countdown.chat.id, countdown.message_id, undefined, `Întrebarea va fi afișată în <b>${i}</b> secunde.`, { parse_mode: 'HTML' })
+        await sleep(1000)
+    }
+    await ctx.telegram.deleteMessage(countdown.chat.id, countdown.message_id)
+
+    var image: Message.PhotoMessage | undefined;
+    if (question?.questionImage != null) {
+        image = await ctx.sendPhoto('https://raw.githubusercontent.com/iamcosmin/drpcivbot/main/assets/image/' + question.questionImage)
+    }
+
+    if (question?.question.length! <= 300 && question?.answ1.length! <= 100 && question?.answ2.length! <= 100 && question?.answ3.length! <= 100) {
+        const quiz = await ctx.sendQuiz(
+            question?.question!,
+            [question?.answ1!, question?.answ2!, question?.answ3!, 'primele două variante', 'ultimele două variante', 'prima și a treia variantă', 'toate trei variante'],
+            {is_anonymous: false, correct_option_id: answerStrToInt(question?.correct), message_thread_id: msg?.message_thread_id, open_period: isCountdown ? 40 : undefined}
+        )
+        return [quiz.message_id];
+    } else {
+        const qText = await ctx.sendMessage(questionText(question), { parse_mode: "HTML" })
+
+        const countdown2 = await ctx.sendMessage(`Întrebarea este prea lungă pentru a fi afișată ca sondaj, așa că este afișată separat. Sondajul va fi afișat în <b>20</b> de secunde.`, { parse_mode: 'HTML' })
+        await sleep(1000)
+        for (var i = 19; i > 0; i--) {
+            await ctx.telegram.editMessageText(countdown2.chat.id, countdown2.message_id, undefined, `Întrebarea este prea lungă pentru a fi afișată ca sondaj, așa că este afișată separat. Sondajul va fi afișat în <b>${i}</b> secunde.`, { parse_mode: 'HTML' })
+            await sleep(1000)
+        }
+        await ctx.telegram.deleteMessage(countdown2.chat.id, countdown2.message_id)
+        const quiz = await ctx.sendQuiz(
+            'Alege varianta corectă.',
+            ['prima variantă', 'a doua variantă', 'a treia variantă', 'primele două variante', 'ultimele două variante', 'prima și a treia variantă', 'toate trei variante'],
+            { is_anonymous: false, correct_option_id: answerStrToInt(question?.correct), message_thread_id: msg?.message_thread_id, open_period: isCountdown ? 40 : undefined},
+        );
+        return [qText.message_id, quiz.message_id]
+    }
+}
+
+async function sendQuiz(ctx: Context) {
+    const oldMsg = ctx.message;
+    await ctx.deleteMessage(ctx.message?.message_id)
+
+    const advise = await ctx.reply(`în 10 de secunde vei începe un chestionar.
+Un chestionar conține 26 de întrebări a câte 40 de secunde fiecare.
+După ce trec cele 40 de secunde, întrebarea va fi ștearsă și una nouă va fi afișată, așa că răspunde repede.
+Succes!`)
+    await sleep(10000)
+    await ctx.deleteMessage(advise.message_id)
+
+    const counter = await ctx.sendMessage('0/26')
+    for (var i = 1; i <= 26; i++) {
+        await ctx.telegram.editMessageText(counter.chat.id, counter.message_id, undefined, `${i}/26`)
+        const question: number[] = await sendQuestion(ctx, counter,  true)
+        await sleep(40000)
+        for (const q in question) {
+            await ctx.deleteMessage(Number.parseInt(q))
+        }
+    }
+    await ctx.sendMessage('Ai terminat chestionarul.')
+}
+
+bot.command('i', async (ctx) => {
+    const msg = ctx.message;
+    await ctx.deleteMessage();
+    sendQuestion(ctx, msg, false)
+})
+
+bot.command('c', async (ctx) => {
+    sendQuiz(ctx)
 })
 
 bot.launch()
